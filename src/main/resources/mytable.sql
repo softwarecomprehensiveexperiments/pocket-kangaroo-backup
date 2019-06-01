5e1d86c79530ee922e01c66e9274d6fcba4e15c4
common89757 <3579836119@qq.com>
2019-06-01 16:59:38 +0800

    建立数据库以及数据表

--- src/main/resources/mytable.sql
+++ src/main/resources/mytable.sql
@@ -1,59 +1,43 @@
 -- auto-generated definition
+create database kangaroo;
+use kangaroo;
 create table users
 (
-    id         int         not null,
-    phone      varchar(11) null,
-    name       varchar(16) not null,
-    password   varchar(16) not null,
-    sex        int         not null,
-    icon       varchar(64) null,
-    properties int         null,
-    lastIP     varchar(16) null,
-    lastDate   date        null,
-    constraint users_id_uindex
-        unique (id),
-    constraint users_name_uindex
-        unique (name),
-    constraint users_password_uindex
-        unique (password),
-    constraint users_phone_uindex
-        unique (phone)
-);
-
-alter table users
-    add primary key (id);
+    user_id         int         not null auto_increment,
+    user_phone      varchar(11) not null,
+    user_name       varchar(16) not null,
+    user_password   varchar(16) not null,
+    user_sex        int         not null,
+    user_icon       varchar(64) null,
+    user_properties int         null,
+    user_lastIp     varchar(16) null,
+    user_lastDate   date        null,
+    primary key (user_id),
+    unique (user_name)
+)engine=InnoDB auto_increment=1 default charset=utf8;
 
 create table task
 (
-    taskId       int          not null,
-    taskTitle    varchar(32)  null,
-    deadLineDate datetime     not null,
-    taskContent  text         not null,
-    price        int          not null,
-    publisherId  int          not null,
-    receiversId  varchar(128) null,
-    publishDate  datetime     not null,
-    taskType     varchar(32)  null,
-    completeDate date         not null,
-    state        int          null,
-    constraint task_taskId_uindex
-        unique (taskId)
-);
-
-alter table task
-    add primary key (taskId);
+    task_id       int          not null auto_increment,
+    task_title    varchar(32)  not null,
+    task_deadLineDate datetime     not null,
+    task_content  text         null,
+    task_price        int          not null,
+    task_publisherId  int          not null,
+    task_receiversId  varchar(128) null,
+    task_publishDate  datetime     not null,
+    task_type     varchar(32)  null,
+    task_completeDate date         not null,
+    task_state        int          null,
+    primary key (task_id)
+)engine=InnoDB auto_increment=1 default charset=utf8;
 
 create table loginlog
 (
-    loginLogId int          not null,
-    userId     int          not null,
-    ip         varchar(128) not null,
-    date       date         not null,
-    constraint LoginLog_loginLogId_uindex
-        unique (loginLogId),
-    constraint LoginLog_userId_uindex
-        unique (userId)
-);
-
-alter table loginlog
-    add primary key (loginLogId);
+    loginLog_id int          not null auto_increment,
+    loginLog_userId     int          not null,
+    loginLog_ip         varchar(128) not null,
+    loginLog_date       date         not null,
+    primary key (loginLog_id),
+    unique (loginLog_userId)
+)engine=InnoDB auto_increment=1 default charset=utf8;
