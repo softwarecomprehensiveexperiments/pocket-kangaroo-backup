629e9559ca77b5ed3ea23f91449deef68939992c
Chenjiff <1004920224@qq.com>
2019-06-03 11:12:25 +0800

    修复bug

---
+++ src/main/java/com/kangaroo/backup/DTO/UpdateUserDTO.java
@@ -0,0 +1,60 @@
+package com.kangaroo.backup.DTO;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class UpdateUserDTO {
+    @JsonProperty("user_name")
+    private String userName;
+
+    @JsonProperty("user_phone")
+    private String userPhone;
+
+    @JsonProperty("user_sex")
+    private int userSex;
+
+    @JsonProperty("old_password")
+    private String userOldPassword;
+
+    @JsonProperty("new_password")
+    private String userNewPassword;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public int getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(int userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserOldPassword() {
+        return userOldPassword;
+    }
+
+    public void setUserOldPassword(String userOldPassword) {
+        this.userOldPassword = userOldPassword;
+    }
+
+    public String getUserNewPassword() {
+        return userNewPassword;
+    }
+
+    public void setUserNewPassword(String userNewPassword) {
+        this.userNewPassword = userNewPassword;
+    }
+}
---
+++ src/main/java/com/kangaroo/backup/Web/UserManagerController.java
@@ -0,0 +1,57 @@
+package com.kangaroo.backup.Web;
+
+import com.kangaroo.backup.DTO.PureStateDTO;
+import com.kangaroo.backup.DTO.TokenPreloadDTO;
+import com.kangaroo.backup.DTO.UpdateUserDTO;
+import com.kangaroo.backup.Domain.User;
+import com.kangaroo.backup.Exception.NoCurrentUserException;
+import com.kangaroo.backup.Service.UserService;
+import com.kangaroo.backup.Utils.FormatCheckerUtils;
+import com.kangaroo.backup.Utils.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 用户个人管理控制器，负责响应用户信息获取，基本信息修改，
+ */
+@RestController
+public class UserManagerController extends BaseController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/user/updateCurrentUser", method = RequestMethod.POST)
+    public PureStateDTO updateUser(@RequestBody UpdateUserDTO updateUserDTO, HttpServletRequest request, HttpServletResponse response) {
+        PureStateDTO pureStateDTO = new PureStateDTO();
+        pureStateDTO.setSuccess(false);
+        boolean isValidPassword = (updateUserDTO.getUserOldPassword() == null && updateUserDTO.getUserNewPassword() == null) ||
+                                    (FormatCheckerUtils.isValidPassword(updateUserDTO.getUserOldPassword()) &&
+                                        FormatCheckerUtils.isValidPassword(updateUserDTO.getUserNewPassword()));
+        if(!FormatCheckerUtils.isValidName(updateUserDTO.getUserName()) ||
+            !FormatCheckerUtils.isValidPhone(updateUserDTO.getUserPhone()) ||
+              !isValidPassword) {
+            pureStateDTO.setDescription("格式不合法");
+            return pureStateDTO;
+        }
+        try {
+            User newUser = userService.updateUser(getCurrentUserId(request), updateUserDTO);
+            if(newUser == null) {
+                pureStateDTO.setDescription("原密码错误");
+                return pureStateDTO;
+            }
+//            TokenPreloadDTO
+//            JWTUtils.
+        } catch (NoCurrentUserException e) {
+            e.printStackTrace();
+            pureStateDTO.setSuccess(false);
+            pureStateDTO.setDescription("认证出现未知错误");
+        }
+        return null;
+    }
+}
